672740408b62c37ee39256125bf64c1c15b8745a
Johannes Charra <johannes.charra@haufe-lexware.net>
2017-02-05 21:47:08 +0100

    fun with flags

--- penta.go
+++ penta.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+)
 
 func main() {
 	fmt.Println("Welcome to Pentago")
+
+	interactive := flag.Bool("i", false, "interactive")
+	port := flag.Int("p", 9977, "Start pentago server listening on this port")
+
+	flag.Parse()
+
+	if *interactive {
+		fmt.Println("Starting interactive play ...")
+	} else {
+		fmt.Println("Start server at port", *port)
+	}
 }
